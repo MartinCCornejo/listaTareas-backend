e67d1719d46bfd91b3a76005269f41a700da68e4
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-12-19 01:08:25 -0300

    Agregamos react-bootstrap y bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
+    <h1 className='text-center'>Hola</h1>
     </>
   )
 }
