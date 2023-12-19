ceedfcbba5fec91aa426933e2be47f6bcdfea329
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-12-19 01:53:16 -0300

    Creamos el componente footer y hicimos la logica para bajarlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-    <h1 className='text-center'>Hola</h1>
+      <Container className="my-4 flex-grow-1">
+        <h1 className="text-center display-3 text-light">Lista tareas</h1>
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="text-center py-3 bg-dark text-light">
+            <p>&copy; Martín Cornejo - Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
