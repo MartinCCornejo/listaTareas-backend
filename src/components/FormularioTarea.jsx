04e6588bc427cbfada7f27f1d4533f9495a7dc9f
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-01-29 14:54:34 -0300

    Guardamos una tarea en el state tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,21 @@
 import { Button, Form } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 function FormularioTarea() {
+  const [tarea, setTarea] = useState("");
+  const [arrayTareas, setArrayTareas] = useState([]);
+
   return (
     <section>
       <Form className="mb-5">
         <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
-          <Form.Control type="text" placeholder="Ej: Tarea 1" />
+          <Form.Control
+            type="text"
+            placeholder="Ej: Tarea 1"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
           <Button variant="dark" type="submit" className="ms-2">
             Agregar
           </Button>{" "}
